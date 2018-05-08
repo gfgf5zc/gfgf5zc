bad4ae4a73f3c66b4d056d689fe9aa620d69a1b0
gfgf5zc <307341383@qq.com>
2018-05-08 10:58:02 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,162 @@
 {
-"employees": [
-{ "firstName":"John" , "lastName":"Doe" },
-{ "firstName":"Anna" , "lastName":"Smith" },
-{ "firstName":"Peter" , "lastName":"Jones" }
+[
+  {
+    "defense": [
+	  "characters": [
+	    {
+		  "cid": 000000,
+		  "rarity": 5
+		},
+		{
+		  "cid": 000001,
+		  "rarity": 4
+		},
+		{
+		  "cid": 000002,
+		  "rarity": 3
+		},
+		{
+		  "cid": 000003,
+		  "rarity": 2
+		},
+		{
+		  "cid": 000004,
+		  "rarity": 1
+		},
+	  ],
+	  "power_level": 52244
+	]
+	"attack": [
+	  {
+	    "characters": [
+		  {
+		    "cid": 000010,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000011,
+			"rarity": 4
+		  },
+		  {
+		    "cid": 000012,
+			"rarity": 3
+		  },
+		  {
+		    "cid": 000013,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000014,
+			"rarity": 5
+		  }
+		],
+		"power_level": 52244
+	  },
+	  {
+	    "characters": [
+		  {
+		    "cid": 000020,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000021,
+			"rarity": 4
+		  },
+		  {
+		    "cid": 000022,
+			"rarity": 3
+		  },
+		  {
+		    "cid": 000023,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000024,
+			"rarity": 5
+		  }
+		],
+		"power_level": 52244
+	  }
+	]
+  }，
+    {
+    "defense": [
+	  "characters": [
+	    {
+		  "cid": 000000,
+		  "rarity": 5
+		},
+		{
+		  "cid": 000001,
+		  "rarity": 4
+		},
+		{
+		  "cid": 000002,
+		  "rarity": 3
+		},
+		{
+		  "cid": 000003,
+		  "rarity": 2
+		},
+		{
+		  "cid": 000004,
+		  "rarity": 1
+		},
+	  ],
+	  "power_level": 52244
+	]
+	"attack": [
+	  {
+	    "characters": [
+		  {
+		    "cid": 000010,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000011,
+			"rarity": 4
+		  },
+		  {
+		    "cid": 000012,
+			"rarity": 3
+		  },
+		  {
+		    "cid": 000013,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000014,
+			"rarity": 5
+		  }
+		],
+		"power_level": 52244
+	  },
+	  {
+	    "characters": [
+		  {
+		    "cid": 000020,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000021,
+			"rarity": 4
+		  },
+		  {
+		    "cid": 000022,
+			"rarity": 3
+		  },
+		  {
+		    "cid": 000023,
+			"rarity": 5
+		  },
+		  {
+		    "cid": 000024,
+			"rarity": 5
+		  }
+		],
+		"power_level": 52244
+	  }
+	]
+  }
 ]
 }
